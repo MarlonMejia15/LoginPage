22e621cd86a284227efa6cd4c09a31c70ba56f9b
MarlonMejia15 <66882055+MarlonMejia15@users.noreply.github.com>
2021-02-28 18:21:46 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from'./components/login.js';
+
+function App() {
+  return (
+    <body>
+    <Login></Login>
+    </body>
+  );
+}
+
+export default App;
